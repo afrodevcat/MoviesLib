c537875c4805d1827feaeff43379f1011470bdfe
Eric Brito <eric_brito@msn.com>
2020-11-12 13:09:08 -0300

    - Commit Inicial

---
+++ MoviesLib/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  MoviesLib
+//
+//  Created by Eric Alves Brito on 11/11/20.
+//  Copyright © 2020 Afrodev. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
---
+++ MoviesLib/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MoviesLib
+//
+//  Created by Eric Alves Brito on 11/11/20.
+//  Copyright © 2020 Afrodev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
