92bf3aea426f3de5b2eec578c8a193c82b7d2740
Eric Brito <eric_brito@msn.com>
2020-11-13 22:27:10 -0300

    - Autolayout parte 2 (traits, priorities) - StackViews

--- MoviesLib/ViewController.swift
+++ MoviesLib/ViewController.swift
@@ -15,6 +15,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func gotToURL(_ sender: UIButton) {
+        UIApplication.shared.open(URL(string: "https://www.toystory3.com.br")!, options: [:], completionHandler: nil)
+    }
+    
 }
 
